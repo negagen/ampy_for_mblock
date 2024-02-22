f36aae08c42e0bca9e956f4926bdc36647937ef4
Egan <negagen@aol.com>
2024-02-22 10:09:03 -0500

    added semi type definition for DeviceContext

--- test.js
+++ test.js
@@ -1,7 +1,50 @@
+// DeviceContext definition
+/**
+ * @typedef {Object} DeviceContext
+ * @property {function} constructor - Constructs a new DeviceContext object.
+ * @property {function} id - Returns the id of the target.
+ * @property {function} isSelected - Checks if the target is selected.
+ * @property {function} getEditingTarget - Returns the editing target.
+ * @property {function} clearBuffer - Clears the buffer of the target.
+ * @property {function} getBufferPool - Returns the buffer pool.
+ * @property {function} deviceID - Returns the device id of the target.
+ * @property {function} getReactor - Returns the reactor.
+ * @property {function} isConnected - Checks if the device is connected.
+ * @property {function} isUploadMode - Checks if the device is in upload mode.
+ * @property {function} switchChannel - Switches the channel.
+ * @property {function} writeRaw - Writes raw data.
+ * @property {function} readRaw - Reads raw data.
+ * @property {function} consumeBuffer - Consumes the buffer.
+ * @property {function} setTextDecoder - Sets the text decoder.
+ * @property {function} getTextDecoder - Returns the text decoder.
+ * @property {function} writeText - Writes text.
+ * @property {function} readText - Reads text.
+ * @property {function} writeHex - Writes hex data.
+ * @property {function} readHex - Reads hex data.
+ * @property {function} clear - Clears the reactor.
+ * @property {function} receiveHexPattern - Receives a hex pattern.
+ * @property {function} subscribeHexPattern - Subscribes to a hex pattern.
+ * @property {function} asyncReadProtocol - Reads a protocol asynchronously.
+ * @property {function} registerProtocol - Registers a protocol.
+ * @property {function} asyncWriteProtocol - Writes a protocol asynchronously.
+ * @property {function} subscribeReadProtocol - Subscribes to read a protocol.
+ * @property {function} subscribeOutput - Subscribes to the output.
+ * @property {function} subscribeFallback - Subscribes to the fallback.
+ * @property {function} uploadFirmware - Uploads firmware.
+ * @property {function} uploadCode - Uploads code.
+ * @property {function} recvData - Receives data.
+ * @property {function} updateSpriteByName - Updates a sprite by name.
+ * @property {function} getByGivenTarget - Gets by given target.
+ * @property {function} getByScratchBlockUtil - Gets by Scratch block util.
+ * @property {function} getByID - Gets by id.
+ * @property {function} getByExtensionID - Gets by extension id.
+ * @property {function} getByConnectedTarget - Gets by connected target.
+ */
+
 // Libraries
 const { Buffer } = require('buffer')
 const { SerialPort } = require('serialport')
-const octabioCommons = require('./dist/bundle.js')
+const octabioCommons = require('./index.js')
 
 // Mocking
 const app = {}
@@ -47,6 +90,7 @@ const device = {
 async function uploadHandler(app, device, code, logHandler, progressHandle, finishHandler, errHandler) {
     // TODO
     try {
+
         const octabioCommons = await loadLibraries()
         logHandler("Creating board interface...")
         const serial = new octabioCommons.MBlockSerial(device);
@@ -65,6 +109,8 @@ async function uploadHandler(app, device, code, logHandler, progressHandle, fini
         logHandler("resetting board");
         serial.write("\x04");
         logHandler(await pyboard.readUntil(1, "soft reboot\r\n"));
+
+
         device.getReactor().setReceiver(()=>{})
         finishHandler(null);
     } catch (er) {
